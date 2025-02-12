df0bd2fdf974d767d689435aa2448cb07d5dde24
Jacob Carlsson <yejcob@gmail.com>
2025-02-12 19:18:32 +0100

    cleanup

--- WF.CustomEvents.Tests/StringPasteTests.cs
+++ WF.CustomEvents.Tests/StringPasteTests.cs
@@ -9,18 +9,15 @@ public class PasteHandlerTests : Bunit.TestContext
     [Test]
     public void ShouldTriggerOnPasteEvent()
     {
-        // Arrange
         PasteEventArgs? receivedArgs = null;
         var cut = RenderComponent<StringPasteEventComponent>(parameters =>
             parameters.Add(p => p.OnPaste, EventCallback.Factory.Create<PasteEventArgs>(this, e => receivedArgs = e))
         );
-        
-        // Act
+
         var pasteEventArgs = new PasteEventArgs { PastedData = "Hello, NUnit!" };
 
         cut.Find("div").TriggerEvent("onstringpaste", pasteEventArgs);
 
-        // Assert
         Assert.That(receivedArgs, Is.Not.Null);
         Assert.That(receivedArgs.PastedData, Is.EqualTo("Hello, NUnit!"));
     }
